ec6fd734606e62782faffd5fe69130dc60abe844
OliverGorylla <78381718+OliverGorylla@users.noreply.github.com>
2021-02-01 21:35:56 +0100

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Beenden_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult msr = MessageBox.Show("Wollen Sie wirklich Beenden?", "Beenden", MessageBoxButton.YesNo);
+
+            if (msr == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
